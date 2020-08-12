7bace69c8cae39005948b86d319d730f13fb87e4
nob <nop.gpk@gmail.com>
2020-08-13 01:31:40 +0700

    clear key

--- app/src/main/java/com/violet/openweather/common/Constant.kt
+++ app/src/main/java/com/violet/openweather/common/Constant.kt
@@ -3,6 +3,6 @@ package com.violet.openweather.common
 class Constant {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-        const val API_KEY = "1ba800e5bc19e46da6df882fd6e8f603"
+        const val API_KEY = ""
     }
 }
